0d3c65442c0bf75acc25d2f02aa24b73f2f296e5
OlenaSMarchenko <olena.smarchenko@gmail.com>
2021-10-15 10:00:18 -0700

    add task 3

--- src/Employee.java
+++ src/Employee.java
@@ -1,18 +1,29 @@
 public class Employee {
-    protected int baseSalary;
-    protected String name;
-    protected int salary;
+    private int baseSalary;
+    private String name;
+    private  int salary;
+
+
+    public Employee(int baseSalary, String name) {
+        this.baseSalary = baseSalary;
+        this.name = name;
+
+    }
 
     public int getBaseSalary() {
         return baseSalary;
     }
+
     public void setBaseSalary(int baseSalary) {
+        this.baseSalary = baseSalary;
     }
 
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
+        this.name = name;
     }
 
     public int getSalary() {
